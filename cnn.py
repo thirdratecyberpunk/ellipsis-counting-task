10a5c4e74061a39dd38d107c2c0f556b1523c19f
thirdratecyberpunk <blackbul@aston.ac.uk>
2019-09-03 15:38:42 +0100

    Added random sampling of training/testing dataset

--- cnn.py
+++ cnn.py
@@ -9,7 +9,13 @@ from skimage import io, transform
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from torch.utils.data.sampler import SubsetRandomSampler
+import torch.nn as nn
+import torch.nn.functional as nnfunc
 
+'''
+Dataset of images and the number of ellipses/other polygons.
+'''
 class EllipsesDataset(Dataset):
     # dataset of images to sort
     def __init__(self, csv_file, root_dir, transform=None):
@@ -37,7 +43,6 @@ class EllipsesDataset(Dataset):
         img_name = os.path.join(self.root_dir,
         str(self.num_of_elements_frame.iloc[idx, 2]),
         str(self.num_of_elements_frame.iloc[idx, 0]))
-        print(img_name)
         polygons = self.num_of_elements_frame.iloc[idx, 1]
         ellipses = self.num_of_elements_frame.iloc[idx, 2]
         sample = {'image': img_name, 'polygons': polygons, 'ellipses': ellipses}
@@ -47,9 +52,28 @@ class EllipsesDataset(Dataset):
 
         return sample
 
-parser= argparse.ArgumentParser(description='Classify an image according to the number of ellipses it contains.')
+'''
+Function that takes returns the training and testing samples
+'''
+def load_split_train_test(transform, valid_size = 0.2):
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    train_idx, test_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    train_loader = torch.utils.data.DataLoader(train_sampler, batch_size=4,
+    shuffle=True, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(test_sampler, batch_size=4,
+    shuffle=True, num_workers=2)
+    return train_loader, test_loader
+
+parser= argparse.ArgumentParser(description=
+'Classify an image according to the number of ellipses it contains.')
 parser.add_argument('--csv_file', default='data/shapes_dataset_MR/labels.csv')
 parser.add_argument('--root_dir', default='data/shapes_dataset_MR/')
+parser.add_argument('--display', default=False)
 
 args = parser.parse_args()
 
@@ -59,23 +83,30 @@ args = parser.parse_args()
 transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
 
-trainset = EllipsesDataset(csv_file=args.csv_file, root_dir=args.root_dir)
+train_data = EllipsesDataset(csv_file=args.csv_file, root_dir=args.root_dir)
+test_data = EllipsesDataset(csv_file=args.csv_file, root_dir=args.root_dir)
 
-# showing images
-fig = plt.figure()
+train_loader, test_loader = load_split_train_test(transform, 0.2)
 
-for i in range(len(trainset)):
-    sample = trainset[i]
-    print (i, sample['image'], sample['polygons'].shape, sample['polygons'].shape)
-    ax = plt.subplot(1, 4, i + 1)
-    plt.tight_layout()
-    img = io.imread(sample['image'])
-    plt.imshow(img)
-    ax.set_title('Sample #{}'.format(i))
-    ax.axis('off')
+# TODO: split dataset into test + training
 
-    if i == 3:
-        plt.show()
-        break
+print(train_loader.dataset)
 
-# loading and displaying the images
+classes = ('0','1','2','3','4','5')
+
+if args.display:
+    # showing images
+    fig = plt.figure()
+
+    for i in range(len(train_data)):
+        sample = train_data[i]
+        ax = plt.subplot(1, 4, i + 1)
+        plt.tight_layout()
+        img = io.imread(sample['image'])
+        plt.imshow(img)
+        ax.set_title('Sample #{}'.format(i))
+        ax.axis('off')
+
+        if i == 3:
+            plt.show()
+            break
